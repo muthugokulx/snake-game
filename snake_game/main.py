1a8d8d368b8d9b3c6debb918996f26e791a6cb12
muthugokulx <muthugokul.sr@gmail.com>
2025-08-30 12:08:24 +0530

    Create Food and Snake classes and implement functionality

--- snake_game/main.py
+++ snake_game/main.py
@@ -1,19 +1,39 @@
-from turtle import Screen,Turtle
+from turtle import Screen
+from snake import Snake
+from food import Food
+import time
+
+
 
 screen = Screen()
+food = Food()
+
 
 screen.title("Snake Game")
 screen.setup(width = 600, height=600)
 screen.bgcolor("black")
+screen.tracer(0)
+
+snake = Snake()
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 
-startingPostion = [(0,0),(-20,0),(-40,0)]
+game_is_on = True
+
+while game_is_on :
+    screen.update() #to refresh screen
+    time.sleep(0.1) 
+    snake.move()
+
+    #Detect collision with food
+    if snake.head.distance(food) < 15 :
+        food.refresh()
 
 
-for position in startingPostion :
-    new_segment = Turtle("square")
-    new_segment.color("white")
-    new_segment.goto(position)
 
 screen.exitonclick()
 
