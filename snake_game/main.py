8564db514d991e4387f000abec68600866ecc952
muthugokulx <muthugokul.sr@gmail.com>
2025-08-30 13:57:52 +0530

    Completed Snake Game Functionality

--- snake_game/main.py
+++ snake_game/main.py
@@ -1,12 +1,14 @@
 from turtle import Screen
 from snake import Snake
 from food import Food
+from scoreboard import Scoreboard
 import time
 
 
 
 screen = Screen()
 food = Food()
+scoreboard = Scoreboard()
 
 
 screen.title("Snake Game")
@@ -32,6 +34,23 @@ while game_is_on :
     #Detect collision with food
     if snake.head.distance(food) < 15 :
         food.refresh()
+        snake.extend()
+        scoreboard.increase_scoreboard()
+
+    #Detect Collision with Wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.xcor() < -280 :
+        game_is_on = False
+        scoreboard.game_over()
+
+    #Detect Collision with Tail
+    for segment in snake.segments[1:] :
+            if snake.head.distance(segment) < 10 :
+                game_is_on = False
+                scoreboard.game_over()
+            
+
+
+
 
 
 
